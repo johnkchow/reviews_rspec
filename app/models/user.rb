6f9309444bc34f6adc111cb47df349b00c782ae8
John Chow <john.chow@teespring.com>
2015-04-29 09:00:40 -0700

    Added User relationship for reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :reviews, foreign_key: :reviewee_id
 end
