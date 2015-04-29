8fbc769f3b544af62a877efc6091a4c7d7167deb
John Chow <john.chow@teespring.com>
2015-04-29 08:59:10 -0700

    Moved the devise_for declaration outside of the api namespace
    Otherwise, all the devise helper methods will require the api prefix i.e. current_api_user

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'homepage#index'
 
-  namespace :api do
-    devise_for :users
+  devise_for :users, controllers: {sessions: "api/sessions"}
 
     resources :users, only: [:create]
   end
